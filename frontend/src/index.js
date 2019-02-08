5b01e01e19059e50ffeed6571a84b998bd4d5055
soulmecca <soulmecca@gmail.com>
2019-02-08 17:50:30 -0500

    add .env with NODE_PATH=src

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, compose } from "redux";
 
-import "./index.css";
-import App from "./App";
-import reducers from "./redux/reducers";
+import "index.css";
+import App from "App";
+import reducers from "redux/reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers());
