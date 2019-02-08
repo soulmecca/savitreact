a24aacdd7a5f5994578cd5b7d62935136587a8b4
soulmecca <soulmecca@gmail.com>
2019-02-08 17:53:32 -0500

    add redux-thunk with applyMiddleware

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, compose } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
 import "index.css";
 import App from "App";
 import reducers from "redux/reducers";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers());
+const store = createStore(
+	reducers,
+	composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
 	<Provider store={store}>
