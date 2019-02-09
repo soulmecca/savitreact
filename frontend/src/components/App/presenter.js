1d9f97930b07e02d7146673beb46ec52215901d8
soulmecca <soulmecca@gmail.com>
2019-02-09 15:52:46 -0500

    add private and public routes for App

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import styles from "./styles.scss";
 import Footer from "components/Footer";
 
-class App extends Component {
-   render() {
-      return (
-         <div className={styles.app}>
-            <Footer />
-         </div>
-      );
-   }
-}
+const App = props => [
+   props.isLoggedIn ? <PrivateRoute key={2} /> : <PublicRoutes key={2} />,
+   <Footer key={3} />
+];
+
+const PrivateRoute = props => (
+   <Switch>
+      <Route path="/" render={() => "feed"} exact />
+      <Route path="/explore" render={() => "explore"} />
+   </Switch>
+);
+
+const PublicRoutes = props => (
+   <Switch>
+      <Route path="/" render={() => "login"} exact />
+      <Route path="/forgot" render={() => "password"} />
+   </Switch>
+);
 
 export default App;
