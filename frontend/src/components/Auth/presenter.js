a3ef8cb3c90a036c2ffc2ead59bac696e034a5aa
soulmecca <soulmecca@gmail.com>
2019-02-09 16:38:58 -0500

    add image files

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,5 +1,31 @@
 import React from "react";
+import styles from "./styles.scss";
 
-const Auth = (props, context) => <h1>whassup!</h1>;
+const Auth = (props, context) => (
+   <main className={StyleSheet.auth}>
+      <div className={styles.column}>
+         <img
+            src={require("images/phone.png")}
+            alt="Checkout our app. Its cool"
+         />
+      </div>
+      <div className={styles.column}>
+         <div className={styles.witeBox} />
+         <div className={styles.appBox}>
+            <span>Get the app</span>
+            <div className={styles.appstores}>
+               <img
+                  src={require("images/ios.png")}
+                  alt="Downlaod it on the Apple Appstroe"
+               />
+               <img
+                  src={require("images/android.png")}
+                  alt="Downlaod it on the Appstore"
+               />
+            </div>
+         </div>
+      </div>
+   </main>
+);
 
 export default Auth;
