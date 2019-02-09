7030c08562b1581a67bd8adc507c6ef6dee60c7f
soulmecca <soulmecca@gmail.com>
2019-02-09 10:16:25 -0500

    removed reactotron

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, compose, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
-import "reactotronConfig";
-import Reactotron from "reactotronConfig";
 
 import "index.css";
 import App from "App";
@@ -13,21 +11,20 @@ import reducers from "redux/reducers";
 const middlewares = [reduxThunk];
 const env = process.env.NODE_ENV;
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-let store;
 
 if (env === "development") {
 	const { logger } = require("redux-logger");
 	middlewares.push(logger);
-	store = Reactotron.createStore(
-		reducers,
-		composeEnhancers(applyMiddleware(...middlewares))
-	);
-} else {
-	store = createStore(
-		reducers,
-		composeEnhancers(applyMiddleware(...middlewares))
-	);
 }
+
+const store = createStore(
+	reducers,
+	composeEnhancers(applyMiddleware(...middlewares))
+);
+
+setTimeout(() => {
+	store.dispatch({ type: "hahaha" });
+});
 
 ReactDOM.render(
 	<Provider store={store}>
--- frontend/src/reactotronConfig.js
+++
@@ -1,8 +0,0 @@
-import Reactotron from "reactotron-react-js";
-import { reactotronRedux } from "reactotron-redux";
-
-Reactotron.configure({ name: "savitreact" })
-	.use(reactotronRedux())
-	.connect(); // let's connect!
-
-export default Reactotron;
