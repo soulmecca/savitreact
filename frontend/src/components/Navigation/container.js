99bb0440dec9169236cc0ea89d89d5fc9ed068f8
soulmecca <soulmecca@gmail.com>
2019-02-12 21:02:20 -0500

    add Navigation component

---
+++ frontend/src/components/Navigation/container.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Navigation from "./presenter";
+
+class NavigationContainer extends Component {
+   render() {
+      return <Navigation />;
+   }
+}
+
+export default NavigationContainer;
