e0f425aacbb53ae9df31a70c681b59e8e14bda4d
soulmecca <soulmecca@gmail.com>
2019-02-15 18:38:29 -0500

    add style to Search component

--- frontend/src/components/Search/presenter.js
+++ frontend/src/components/Search/presenter.js
@@ -3,12 +3,37 @@ import PropTypes from "prop-types";
 import styles from "./styles.scss";
 import Loading from "components/Loading";
 import UserDisplay from "components/UserDisplay";
+// import PhotoDisplay from "components/PhotoDisplay";
 
-const Search = props => {
-   if (props.loading) {
-      return <LoadingSearch />;
-   } else {
-   }
+const Search = (props, context) => {
+   return (
+      <div className={styles.search}>
+         <div className={styles.section}>
+            <h4 className={styles.title}>{context.t("Users")}</h4>
+            {props.loading && <LoadingSearch />}
+            {!props.loading && props.users.length < 1 && (
+               <NotFound text={context.t("Nothing found")} />
+            )}
+            <div className={styles.content}>
+               {!props.loading && props.users.length > 0 && (
+                  <RenderUserSearch users={props.users} />
+               )}
+            </div>
+         </div>
+         <div className={styles.section}>
+            <h4 className={styles.title}>{context.t("Images")}</h4>
+            {props.loading && <LoadingSearch />}
+            {!props.loading && props.posts.length < 1 && (
+               <NotFound text={context.t("Nothing found")} />
+            )}
+            <div className={styles.content}>
+               {!props.loading && props.posts.length > 0 && (
+                  <RenderImageSearch posts={props.posts} />
+               )}
+            </div>
+         </div>
+      </div>
+   );
 };
 
 const LoadingSearch = () => (
@@ -16,6 +41,19 @@ const LoadingSearch = () => (
       <Loading />
    </div>
 );
+
+const RenderUserSearch = props =>
+   props.users.map(user => (
+      <UserDisplay vertical={true} user={user} key={user.id} />
+   ));
+
+const RenderImageSearch = props => props.posts.map(post => post.caption);
+
+const NotFound = props => <span className={styles.notFound}>{props.text}</span>;
+
+Search.contextTypes = {
+   t: PropTypes.func.isRequired
+};
 
 Search.propTypes = {
    loading: PropTypes.bool.isRequired
