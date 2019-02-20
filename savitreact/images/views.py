45575b312db91bb8edf95bb4a302dd718ba65ea1
soulmecca <soulmecca@gmail.com>
2019-02-20 15:59:52 -0500

    add context to Search view

--- savitreact/images/views.py
+++ savitreact/images/views.py
@@ -162,13 +162,14 @@ class Search(APIView):
 
             images = models.Image.objects.filter(tags__name__in=hashtags).distinct()
 
-            serializer = serializers.UserProfileImageSerializer(images, many=True)
+            serializer = serializers.UserProfileImageSerializer(images, many=True, context={'request': request})
 
             return Response(data=serializer.data, status=status.HTTP_200_OK)
 
         else:
-
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            images = models.Image.objects.all()[:20]
+            serializer = serializers.UserProfileImageSerializer(images, many=True, context={'request': request})
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
 class ModerateCommentOnImage(APIView):
