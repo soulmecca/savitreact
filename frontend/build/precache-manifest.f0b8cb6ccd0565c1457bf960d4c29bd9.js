b6394fa025d419e61047c2054318454a0f87aedc
soulmecca <soulmecca@gmail.com>
2019-02-25 14:43:25 -0500

    add public url

---
+++ frontend/build/precache-manifest.f0b8cb6ccd0565c1457bf960d4c29bd9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d17c0469c60375a01091fcee28f3749",
+    "url": "https://savitreact.s3.amazonaws.com/static/media/phone.5d17c046.png"
+  },
+  {
+    "revision": "667102992c39168c86b0a4939b8ae77c",
+    "url": "https://savitreact.s3.amazonaws.com/static/media/logo.66710299.png"
+  },
+  {
+    "revision": "f06b908907d5d4f2aaf733e2bee7ea8e",
+    "url": "https://savitreact.s3.amazonaws.com/static/media/android.f06b9089.png"
+  },
+  {
+    "revision": "4261c52df95e66eae0d7",
+    "url": "https://savitreact.s3.amazonaws.com/static/js/runtime~main.4261c52d.js"
+  },
+  {
+    "revision": "ca14e37a7d59c9bb47bd",
+    "url": "https://savitreact.s3.amazonaws.com/static/js/main.ca14e37a.chunk.js"
+  },
+  {
+    "revision": "c2daa43086c5714ee710",
+    "url": "https://savitreact.s3.amazonaws.com/static/js/1.c2daa430.chunk.js"
+  },
+  {
+    "revision": "ca14e37a7d59c9bb47bd",
+    "url": "https://savitreact.s3.amazonaws.com/static/css/main.cfc37d97.chunk.css"
+  },
+  {
+    "revision": "605063f92565ce740011023b96e6c120",
+    "url": "https://savitreact.s3.amazonaws.com/index.html"
+  }
+];
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f236e5b66392c0b92e6a46dba2cfd50c.js"
+  "https://savitreact.s3.amazonaws.com/precache-manifest.f0b8cb6ccd0565c1457bf960d4c29bd9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("https://savitreact.s3.amazonaws.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
