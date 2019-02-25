6d0fbd790f6fa6eb6dd4a9116ceae06a3d4e66f6
soulmecca <soulmecca@gmail.com>
2019-02-24 21:44:06 -0500

    add * to allowed_hosts in local.py

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,13 +8,7 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='tk4rLz9VnYCYEQPYXKmXfRSMi3nmDnlwFc1TpHAbGCzeQoNevIP0gyQvJru2hphA')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [
-    "*",
-    "localhost",
-    "0.0.0.0",
-    "127.0.0.1",
-    'savitreact.us-east-1.elasticbeanstalk.com'
-]
+ALLOWED_HOSTS = ["*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,7 @@ from .base import env
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 # ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['example.com'])
-ALLOWED_HOSTS = ['*', 'localhost', 'savitreact.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['*']
 
 
 # DATABASES
