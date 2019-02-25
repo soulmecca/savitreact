d71c6c2d0b7fabd442930cda3bafa268485a8070
soulmecca <soulmecca@gmail.com>
2019-02-25 13:24:14 -0500

    add ebuser.py

--- savitreact/users/management/commands/ebuser.py
+++ savitreact/users/management/commands/ebuser.py
@@ -1,10 +1,9 @@
-# from django.core.management.base import BaseCommand
-# from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
 
 
-# class Command(BaseCommand):
-
-#     def handle(self, *args, **options):
-
-#         User = get_user_model()
-#         User.objects.create_superuser('admin', 'soulmecca@gmail.com', '1234')
+class Command(BaseCommand):
+  def handle(self, *args, **options):
+  	User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+    	User.objects.create_superuser('admin', 'soulmecca@gmail.com', '1234')
