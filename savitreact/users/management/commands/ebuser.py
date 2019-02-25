486d9d68127b3bbaf58800b03e0dbb33db158158
soulmecca <soulmecca@gmail.com>
2019-02-25 10:33:59 -0500

    changed 20_python.config

--- savitreact/users/management/commands/ebuser.py
+++ savitreact/users/management/commands/ebuser.py
@@ -1,10 +1,10 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth import get_user_model
+# from django.core.management.base import BaseCommand
+# from django.contrib.auth import get_user_model
 
 
-class Command(BaseCommand):
+# class Command(BaseCommand):
 
-    def handle(self, *args, **options):
+#     def handle(self, *args, **options):
 
-        User = get_user_model()
-        User.objects.create_superuser('admin', 'soulmecca@gmail.com', '1234')
+#         User = get_user_model()
+#         User.objects.create_superuser('admin', 'soulmecca@gmail.com', '1234')
